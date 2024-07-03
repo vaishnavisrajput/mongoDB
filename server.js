8d77e6951bbb73904359eebd6b936c819f51a902
Vaishnavi Singh Rajput <vaishnavirajput981@gmail.com>
2024-07-03 19:25:04 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const db = require('./db');
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World, welcome to my server')
+})
+
+
+
+app.listen(3000, ()=>{
+    console.log("Server is working");
+})  
